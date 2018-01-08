9e273ad7ea7f5b320d5565c7661652ba7447476b
Victor Palade <palade.ionut@gmail.com>
2018-01-08 15:02:33 +0100

    routing: add basic index and healthz routes.

--- config/routes.js
+++ config/routes.js
@@ -1,14 +1,9 @@
-module.exports = {
+module.exports = (app) => {
+  app.get('/', (req, res) => {
+    res.send('Hello World!')
+  });
 
-  // index is the main entrypoint of the application, it will serve the landing
-  // page.
-  index: (res, req)=> {
-    res.send('Hello World')
-  }
-
-  // healtzh is just a basic endpoint that tells us that the application is
-  // responsive.
-  healtzh: (res, req)=> {
+  app.get('/healthz', (req, res) => {
     res.sendStatus(200)
-  }
+  });
 }
--- server.js
+++ server.js
@@ -1,13 +1,27 @@
+// dotenv loads our env variables from the .env file.
 require('dotenv').config()
+// express is our http server.
 const express = require('express');
+// colors add highlighting to important operations to make it easier to debug
+// the app.
 const colors = require('colors');
+// handlebars is the templating engine used for dynamic html rendering.
 const handlebars = require('handlebars');
+// we use the path to join and create canonical paths to our application
+// components.
 const path = require('path');
 const app = express();
 
+require('routes')(app);
+// get current working directory of the application.
 const rootPath = path.dirname(path.resolve(__dirname));
+// here we concatenate our component names with our root directory to have
+// canonical paths to them and make it easy to reference and load throughout the
+// application.
 const viewPath = path.join(rootPath, '/views');
 
+// create a basic server that will try and load the env variables defined in the
+// .env file, otherwise it will fallback to some default hardcoded values.
 const server = app.listen(process.env.PORT || 3000, process.env.HOST || '127.0.0.1', ()=> {
   const host = server.address().address;
   const port = server.address().port;
