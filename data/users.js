11c35fd8c9fe987a7ce8d5971abb7382094d6f86
Victor Palade <palade.ionut@gmail.com>
2018-01-08 17:25:50 +0100

    data: addd some basic data source

---
+++ data/users.js
@@ -0,0 +1,31 @@
+{
+  "users": {
+    "Victor Palade": {
+      "company": "adidas AG",
+      "position": "Software Engineer",
+      "skills": [
+        "go",
+        "containers",
+        "virtualization"
+      ]
+    },
+    "John Doe": {
+      "company": "facebook",
+      "position": "Software Engineer",
+      "skills": [
+        "html",
+        "javascript",
+        "php"
+      ]
+    },
+    "Jane Doe": {
+      "company": "Google",
+      "position": "Software Engineer",
+      "skills": [
+        "type script",
+        "react/redux",
+        "jQuery"
+      ]
+    }
+  }
+}
