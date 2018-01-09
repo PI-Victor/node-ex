11883596895951846b22dd7b9b0ec95a231af1b7
Victor Palade <palade.ionut@gmail.com>
2018-01-09 12:17:39 +0100

    tests: add basic tests for the two existing endpoints.

---
+++ tests/server_test.js
@@ -0,0 +1,27 @@
+const assert = require('assert');
+const express = require('express');
+const request = require('supertest');
+const app = express();
+
+// Here is where, spliting the application configuration from the server init
+// code comes in handy. We can now, test the application with the actual
+// settings that we have configured in the real app, instead of creating an
+// exact replica.
+require('application')(app);
+require('routes')(app);
+
+app.listen(3030);
+
+describe('Basic application endpoint test', ()=> {
+  it('GET to index should return 200', (done)=> {
+    request(app)
+      .get('/')
+      .expect(200, done);
+  });
+
+  it('GET to /healthz should return 200', (done)=> {
+    request(app)
+      .get('/healthz')
+      .expect(200, done);
+  });
+});
